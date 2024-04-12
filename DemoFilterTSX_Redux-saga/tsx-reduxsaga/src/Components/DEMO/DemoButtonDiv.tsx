e924dddae9359b677d74bdb6b97bdcd40f478db1
dolong <dolong212000@gmail.com>
2024-04-12 17:24:48 +0700

    WIP

--- DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/Components/DEMO/DemoButtonDiv.tsx
+++ DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/Components/DEMO/DemoButtonDiv.tsx
@@ -1,27 +1,32 @@
 import React, { useState } from 'react';
-
+import './demo.scss';
+import { FormLayout, TextField } from '@shopify/polaris';
 function DemoButtonDiv(props: any) {
-    const [selectedDiv, setSelectedDiv] = useState<number | null>(null);
-    const handleDivClick = (index: number) => {
-        setSelectedDiv(index);
-      };
+    
     return (
-        <div style={{display: 'flex'}}>
-            <div>
-            <div style={{width: '100px', height: '100px', border: '1px solid red', backgroundColor: selectedDiv === 1 ? 'gray' : 'transparent',}} onClick={() => handleDivClick(1)}>1</div>
-            <div style={{width: '100px', height: '100px', border: '1px solid red', backgroundColor: selectedDiv === 2 ? 'gray' : 'transparent',}} onClick={() => handleDivClick(2)}>2</div>
-            <div style={{width: '100px', height: '100px', border: '1px solid red'}} onClick={() => handleDivClick(3)}>3</div>
-            <div style={{width: '100px', height: '100px', border: '1px solid red'}} onClick={() => handleDivClick(4)}>4</div>
-            <div style={{width: '100px', height: '100px', border: '1px solid red'}} onClick={() => handleDivClick(5)}>5</div>
-            </div>
-            <div>
-            <div style={{width: '100px', height: '100px', border: '1px solid red'}} onClick={() => handleDivClick(6)}>6</div>
-            <div style={{width: '100px', height: '100px', border: '1px solid red'}} onClick={() => handleDivClick(7)}>7</div>
-            <div style={{width: '100px', height: '100px', border: '1px solid red'}} onClick={() => handleDivClick(8)}>8</div>
-            <div style={{width: '100px', height: '100px', border: '1px solid red'}} onClick={() => handleDivClick(9)}>9</div>
-            <div style={{width: '100px', height: '100px', border: '1px solid red'}} onClick={() => handleDivClick(10)}>10</div>
-            </div>
-        </div>
+<FormLayout>
+      <FormLayout.Group condensed>
+        {/* <TextField label="Length" onChange={() => {}} autoComplete="off" />
+        <TextField label="Width" onChange={() => {}} autoComplete="off" />
+        <TextField label="Height" onChange={() => {}} autoComplete="off" />
+        <TextField label="Unit" onChange={() => {}} autoComplete="off" /> */}
+
+                          
+                          {/* Điện thoại */}
+                          <div style={{ marginTop: '20px', display: 'flex', alignItems: 'center' }}>
+                            <label className='labelName' style={{ width: '8.5%' }}>
+                              Điện thoại<span className='labelNameSpan'>*</span>
+                            </label>
+                            <input className='phoneAreaCode' type='text' value='+84' readOnly />
+                            <input
+                              className='phoneNumber'
+                              type='tel'
+                              placeholder='Nhập số điện thoại của bạn'
+                              value='0943123123'
+                            />
+                          </div>
+      </FormLayout.Group>
+    </FormLayout>
     );
 }
 
