6e5ef090aa4b671d7e5a67f37af7d11e088942b0
dolong <dolong212000@gmail.com>
2024-04-15 17:26:17 +0700

    DEMO

--- DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/App.tsx
+++ DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/App.tsx
@@ -5,6 +5,7 @@ import DemoCreateNewAddressLastest from 'Components/DEMO/DemoCreateNewAddressLas
 import Demo63citys from 'Components/DEMO/Demo63citys';
 import DemoFixed from 'Components/DEMO/DemoFixed';
 import DemoButtonDiv from 'Components/DEMO/DemoButtonDiv';
+import TopBarExample from 'Components/DEMO/Demo2';
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
 {/* <Demo63citys/> */}
 {/* <DemoFixed/> */}
 <DemoButtonDiv/>
+<TopBarExample/>
     </div>
   );
 }
