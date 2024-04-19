04621c6b765c7478501052068d29c1094f5eee42
dolong <dolong212000@gmail.com>
2024-04-19 10:19:12 +0700

    Demo

--- DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/App.tsx
+++ DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/App.tsx
@@ -1,19 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '@shopify/polaris/build/esm/styles.css';
 import './App.css';
-import DemoCreateNewAddressLastest from 'Components/DEMO/DemoCreateNewAddressLastest';
-import Demo63citys from 'Components/DEMO/Demo63citys';
-import DemoFixed from 'Components/DEMO/DemoFixed';
-import DemoButtonDiv from 'Components/DEMO/DemoButtonDiv';
-import TopBarExample from 'Components/DEMO/Demo2';
 function App() {
   return (
     <div className="App">
-{/* <DemoCreateNewAddressLastest/> */}
-{/* <Demo63citys/> */}
-{/* <DemoFixed/> */}
-<DemoButtonDiv/>
-<TopBarExample/>
+
     </div>
   );
 }
