e27037693546ecd48b27d55f2b808e857b9fae1e
dolong <dolong212000@gmail.com>
2024-04-22 16:39:01 +0700

    WIP: List_Item

--- DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/App.tsx
+++ DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '@shopify/polaris/build/esm/styles.css';
 import './App.css';
+import Pagination from 'Components/Demo/DemoScrollXListItem';
+import DemoScrollXListItem from 'Components/Demo/DemoScrollXListItem';
+import App1 from 'Components/Demo/DemoScrollXListItem';
+import InfiniteLoop from 'Components/Demo/DemoScrollXListItem';
+// import DemoScrollXListItem from 'Components/Demo/DemoScrollXListItem';
 function App() {
   return (
     <div className="App">
-
+<InfiniteLoop />
     </div>
   );
 }
