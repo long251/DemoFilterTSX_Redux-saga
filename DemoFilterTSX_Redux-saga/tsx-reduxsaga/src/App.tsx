2ff9987a8ae1f3784aa6e5fc7f1e51fcd281709d
dolong <dolong212000@gmail.com>
2024-04-23 17:23:39 +0700

    Demo Shopping

--- DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/App.tsx
+++ DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import '@shopify/polaris/build/esm/styles.css';
 import './App.css';
-import Pagination from 'Components/Demo/DemoScrollXListItem';
-import DemoScrollXListItem from 'Components/Demo/DemoScrollXListItem';
-import App1 from 'Components/Demo/DemoScrollXListItem';
-import InfiniteLoop from 'Components/Demo/DemoScrollXListItem';
+import DemoIcon from 'Components/Demo/DemoMini/DemoIcon';
+import InfiniteLoop from 'Components/Demo/DemoLarge/DemoScrollXListItem';
+import DemoNhapNho from 'Components/Demo/DemoMini/DemoNhapNho';
 // import DemoScrollXListItem from 'Components/Demo/DemoScrollXListItem';
 function App() {
   return (
     <div className="App">
-<InfiniteLoop />
+      {/* <DemoIcon /> */}
+      {/* <InfiniteLoop/> */}
+      <DemoNhapNho/>
     </div>
   );
 }
---
+++ DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/Components/Demo/DemoMini/DemoNhapNho.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DemoNhapNho(props:any) {
+    return (
+        <div>
+            <div>1</div>
+            <div>2</div>
+        </div>
+    );
+}
+
+export default DemoNhapNho;
