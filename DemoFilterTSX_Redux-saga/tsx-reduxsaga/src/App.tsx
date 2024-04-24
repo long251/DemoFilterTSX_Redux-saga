e6e1b88ef3634d00a47e43ce917b07931503ea7f
dolong <dolong212000@gmail.com>
2024-04-24 17:16:06 +0700

    WIP: demo shopping

--- DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/App.tsx
+++ DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import '@shopify/polaris/build/esm/styles.css';
 import './App.css';
-import DemoIcon from 'Components/Demo/DemoMini/DemoIcon';
-import InfiniteLoop from 'Components/Demo/DemoLarge/DemoScrollXListItem';
-import DemoNhapNho from 'Components/Demo/DemoMini/DemoNhapNho';
+
 // import DemoScrollXListItem from 'Components/Demo/DemoScrollXListItem';
 function App() {
   return (
     <div className="App">
-      {/* <DemoIcon /> */}
-      {/* <InfiniteLoop/> */}
-      <DemoNhapNho/>
+
     </div>
   );
 }
