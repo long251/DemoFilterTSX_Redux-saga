2767dc65ff57bed45464f35715e4cdd4bbace95c
dolong <dolong212000@gmail.com>
2024-05-13 16:04:59 +0700

    Demo_empty

--- DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/App.tsx
+++ DemoFilterTSX_Redux-saga/tsx-reduxsaga/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '@shopify/polaris/build/esm/styles.css';
 import './App.css'; 
+import DemoWrap from 'Components/Demo/DemoWrap';
 
 // import DemoScrollXListItem from 'Components/Demo/DemoScrollXListItem';
 function App() {
   return (
     <div className="App">
-
+      <DemoWrap/>
     </div>
   );
 }
